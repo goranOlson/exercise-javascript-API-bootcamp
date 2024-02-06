2f9aa876cbc442f5e5c6277dfcb76f00fa0e35aa
Göran Olson <goran@famolson.se>
2024-02-06 15:50:55 +0100

    added exercise 2

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-/* 1 */
+/* 1.1 */
 // fetch('https://majazocom.github.io/Data/pokemons.json')
 //     .then((response) => response.text())
 //     .then((data) => console.log(data))
 //     .catch((error) => console.log('Error:', error)); 
 
-/* 2 */
+/* 1.2 */
 // fetch('https://majazocom.github.io/Data/pokemons.json')
 //     .then((response) => response.json())
 //     .then((data) => {
@@ -14,16 +14,33 @@
 //     })
 //     .catch((error) => console.log('Error:', error));
 
-/* 3 */
-const divOutput = document.getElementById('output');
+/* 1.3 */
+// const divOutput = document.getElementById('output');
 
-fetch('https://majazocom.github.io/Data/pokemons.json')
+// fetch('https://majazocom.github.io/Data/pokemons.json')
+//     .then((response) => response.json())
+//     .then((data) => {
+//         data.forEach(element => {
+//             //  console.log(element);
+//             const item = `<div class='item'>${element.name}</div>`;
+//             divOutput.insertAdjacentHTML('beforeend', item);
+//         });
+//     })
+//     .catch((error) => console.error('Error:', error));
+
+
+
+/* 2.1  - 2.3 */
+
+const output = document.getElementById('output');
+
+fetch('https://majazocom.github.io/Data/dogs.json')
     .then((response) => response.json())
     .then((data) => {
-        data.forEach(element => {
-            //  console.log(element);
-            const item = `<div class='item'>${element.name}</div>`;
-            divOutput.insertAdjacentHTML('beforeend', item);
+        // data.forEach(dog => { console.log(dog.name) })
+        data.forEach(dog => {
+            const item = `<div class="item">${dog.name}</div>`;
+            output.insertAdjacentHTML('beforeend', item);
         });
     })
-    .catch((error) => console.log('Error:', error));
+    .catch((error) => console.error('Error:', error) )
